334b5f2e3e2e2970ee12d414e15c2b63bc97a507
WillieMaddox <willie.maddox@gmail.com>
2016-04-17 01:03:53 -0500

    check-in

--- web2/ol3-wfs-t.js
+++ web2/ol3-wfs-t.js
@@ -135,15 +135,15 @@ var formatGML = new ol.format.GML({
 });
 var transactWFS = function(p, f) {
 	switch(p) {
-	case 'insert':
-		node = formatWFS.writeTransaction([f], null, null, formatGML);
-		break;
-	case 'update':
-		node = formatWFS.writeTransaction(null, [f], null, formatGML);
-		break;
-	case 'delete':
-		node = formatWFS.writeTransaction(null, null, [f], formatGML);
-		break;
+		case 'insert':
+			node = formatWFS.writeTransaction([f], null, null, formatGML);
+			break;
+		case 'update':
+			node = formatWFS.writeTransaction(null, [f], null, formatGML);
+			break;
+		case 'delete':
+			node = formatWFS.writeTransaction(null, null, [f], formatGML);
+			break;
 	}
 	s = new XMLSerializer();
 	str = s.serializeToString(node);
@@ -157,8 +157,8 @@ var transactWFS = function(p, f) {
 }
 
 $('.btn-floating').hover(
-	function() {$(this).addClass('darken-2');},
-	function() {$(this).removeClass('darken-2');}
+	function() { $(this).addClass('darken-2'); },
+	function() { $(this).removeClass('darken-2'); }
 );
 
 $('.btnMenu').on('click', function(event) {
@@ -167,100 +167,100 @@ $('.btnMenu').on('click', function(event) {
 	map.removeInteraction(interaction);
 	select.getFeatures().clear();
 	map.removeInteraction(select);
-	switch($(this).attr('id')) {
+	switch ($(this).attr('id')) {
 	
-	case 'btnSelect':
-		interaction = new ol.interaction.Select({
-			style: new ol.style.Style({
-				stroke: new ol.style.Stroke({color: '#f50057', width: 2})
-			})
-		});
-		map.addInteraction(interaction);
-		/*interaction.getFeatures().on('add', function(e) {
-			props = e.element.getProperties();
-			if (props.status){$('#popup-status').html(props.status);}else{$('#popup-status').html('n/a');}
-			if (props.tiendas){$('#popup-tiendas').html(props.tiendas);}else{$('#popup-tiendas').html('n/a');}
-			coord = $('.ol-mouse-position').html().split(',');
-			overlayPopup.setPosition(coord);
-		});*/
-		break;
-		
-	case 'btnEdit':
-		map.addInteraction(select);
-		interaction = new ol.interaction.Modify({
-			features: select.getFeatures()
-		});
-		map.addInteraction(interaction);
-		
-		snap = new ol.interaction.Snap({
-			source: layerVector.getSource()
-		});
-		map.addInteraction(snap);
-		
-		dirty = {};
-		select.getFeatures().on('add', function(e) {
-			e.element.on('change', function(e) {
-				dirty[e.target.getId()] = true;
+		case 'btnSelect':
+			interaction = new ol.interaction.Select({
+				style: new ol.style.Style({
+					stroke: new ol.style.Stroke({ color: '#f50057', width: 2 })
+				})
 			});
-		});
-		select.getFeatures().on('remove', function(e) {
-			f = e.element;
-			if (dirty[f.getId()]){
-				delete dirty[f.getId()];
-				featureProperties = f.getProperties();
-			    delete featureProperties.boundedBy;
-			    var clone = new ol.Feature(featureProperties);
-			    clone.setId(f.getId());
-			    transactWFS('update', clone);
-			}
-		});
-		break;
-		
-	case 'btnDrawPoint':
-		interaction = new ol.interaction.Draw({
-		    type: 'Point',
-		    source: layerVector.getSource()
-		});
-		map.addInteraction(interaction);
-		interaction.on('drawend', function(e) {
-			transactWFS('insert', e.feature);
-	    });
-		break;
-		
-	case 'btnDrawLine':
-		interaction = new ol.interaction.Draw({
-		    type: 'LineString',
-		    source: layerVector.getSource()
-		});
-		map.addInteraction(interaction);
-		interaction.on('drawend', function(e) {
-			transactWFS('insert', e.feature);
-	    });
-		break;
-		
-	case 'btnDrawPoly':
-		interaction = new ol.interaction.Draw({
-		    type: 'Polygon',
-		    source: layerVector.getSource()
-		});
-		map.addInteraction(interaction);
-		interaction.on('drawend', function(e) {
-			transactWFS('insert', e.feature);
-	    });
-		break;
-		
-	case 'btnDelete':
-		interaction = new ol.interaction.Select();
-		map.addInteraction(interaction);
-		interaction.getFeatures().on('change:length', function(e) {
-			transactWFS('delete', e.target.item(0));
-	        interaction.getFeatures().clear();
-	        selectPointerMove.getFeatures().clear();
-	    });
-		break;
+			map.addInteraction(interaction);
+			/*interaction.getFeatures().on('add', function(e) {
+				props = e.element.getProperties();
+				if (props.status){$('#popup-status').html(props.status);}else{$('#popup-status').html('n/a');}
+				if (props.tiendas){$('#popup-tiendas').html(props.tiendas);}else{$('#popup-tiendas').html('n/a');}
+				coord = $('.ol-mouse-position').html().split(',');
+				overlayPopup.setPosition(coord);
+			});*/
+			break;
 
-	default:
-		break;
+		case 'btnEdit':
+			map.addInteraction(select);
+			interaction = new ol.interaction.Modify({
+				features: select.getFeatures()
+			});
+			map.addInteraction(interaction);
+
+			snap = new ol.interaction.Snap({
+				source: layerVector.getSource()
+			});
+			map.addInteraction(snap);
+
+			dirty = {};
+			select.getFeatures().on('add', function(e) {
+				e.element.on('change', function(e) {
+					dirty[e.target.getId()] = true;
+				});
+			});
+			select.getFeatures().on('remove', function(e) {
+				f = e.element;
+				if (dirty[f.getId()]){
+					delete dirty[f.getId()];
+					featureProperties = f.getProperties();
+					delete featureProperties.boundedBy;
+					var clone = new ol.Feature(featureProperties);
+					clone.setId(f.getId());
+					transactWFS('update', clone);
+				}
+			});
+			break;
+
+		case 'btnDrawPoint':
+			interaction = new ol.interaction.Draw({
+				type: 'Point',
+				source: layerVector.getSource()
+			});
+			map.addInteraction(interaction);
+			interaction.on('drawend', function(e) {
+				transactWFS('insert', e.feature);
+			});
+			break;
+
+		case 'btnDrawLine':
+			interaction = new ol.interaction.Draw({
+				type: 'LineString',
+				source: layerVector.getSource()
+			});
+			map.addInteraction(interaction);
+			interaction.on('drawend', function(e) {
+				transactWFS('insert', e.feature);
+			});
+			break;
+
+		case 'btnDrawPoly':
+			interaction = new ol.interaction.Draw({
+				type: 'Polygon',
+				source: layerVector.getSource()
+			});
+			map.addInteraction(interaction);
+			interaction.on('drawend', function(e) {
+				transactWFS('insert', e.feature);
+			});
+			break;
+
+		case 'btnDelete':
+			interaction = new ol.interaction.Select();
+			map.addInteraction(interaction);
+			interaction.getFeatures().on('change:length', function(e) {
+				transactWFS('delete', e.target.item(0));
+				interaction.getFeatures().clear();
+				selectPointerMove.getFeatures().clear();
+			});
+			break;
+
+		default:
+			break;
 	}
 });
 
