9640a615c85827d0c9ba863841a6c4455d0d2835
Willie Maddox <willie.maddox@gmail.com>
2016-05-12 06:26:43 -0500

    Change WFS code

--- scripts/vagrant/geoserver.sh
+++ scripts/vagrant/geoserver.sh
@@ -3,15 +3,23 @@
 export DEBIAN_FRONTEND=noninteractive
 
 #db
-apt-get install -qqy flex autoconf libfcgi-dev libapache2-mod-fastcgi 
+apt-get install default-jdk default-jre maven git
+
+#apt-get install -qqy flex autoconf libfcgi-dev libapache2-mod-fastcgi 
 #libapache2-mod-auth-pgsql 
 # use one or the other but not both.
 #apt-get install -qqy libapache2-mod-python
 #apt-get install -qqy libapache2-mod-wsgi
+
 #db
-apt-get install -qqy libfreetype6-dev libxml2-dev libxslt1-dev libtiff-dev libtiff5-dev libpng12-dev libpng-dev libjpeg8-dev
+# apt-get install -qqy libfreetype6-dev libxml2-dev libxslt1-dev libtiff-dev libtiff5-dev libpng12-dev libpng-dev libjpeg8-dev
 #install postgresql postgis and gdal
-apt-get install -qqy libgdal-dev gdal-bin python-gdal postgresql-9.4 postgresql-contrib postgresql-9.4-pgrouting postgresql-9.4-postgis-2.1 postgis binutils libproj-dev libpq-dev
+# apt-get install -qqy libgdal-dev gdal-bin python-gdal postgresql-9.4 postgresql-contrib postgresql-9.4-pgrouting postgresql-9.4-postgis-2.1 postgis binutils libproj-dev libpq-dev
+
+git clone https://github.com/geoserver/geoserver.git
+cd geoserver
+mvn clean install
+ 
 
 PG_VERSION=9.4
 PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
@@ -25,8 +33,11 @@ sed -i  's/peer/trust/' "$PG_HBA"
 
 service postgresql restart
 
-mkdir -p /vagrant/third_party
-cd /vagrant/third_party
+cd /vagrant
+if [[ ! -d third_party ]]; then
+  mkdir third_party
+fi
+cd third_party
 if [[ ! -d tinyows ]]; then
   git clone https://github.com/WillieMaddox/tinyows.git
 fi
@@ -48,7 +59,9 @@ psql -U postgres -d tinyows < `pg_config --sharedir`/contrib/postgis-2.1/topolog
 psql -U postgres -d tinyows < `pg_config --sharedir`/contrib/postgis-2.1/rtpostgis.sql
 
 # Download some sample data and populate the database with it.
-mkdir -p /vagrant/default_data
+if [[ ! -d /vagrant/default_data ]]; then
+  mkdir /vagrant/default_data
+fi
 cd /vagrant/default_data
 if [[ ! -f frida-1.0.1-shp.tar.gz ]]; then
   wget ftp://ftp.intevation.de/freegis/frida/frida-1.0.1-shp.tar.gz
--- web2/ol3-wfs-t.js
+++ web2/ol3-wfs-t.js
@@ -1,277 +1,265 @@
 controlMousePos = new ol.control.MousePosition({
-	coordinateFormat: ol.coordinate.createStringXY(4),
+    coordinateFormat: ol.coordinate.createStringXY(4),
 });
 
-/*
 popup = document.getElementById('popup');
 $('#popup-closer').on('click', function() {
-	overlayPopup.setPosition(undefined);
+    overlayPopup.setPosition(undefined);
 });
 overlayPopup = new ol.Overlay({
-	element: popup
+    element: popup
 });
-*/
 
-/*
-var u="http://aspe.local.db0/cgi-bin/tinyows?service=wfs&version=1.1.0&request=DescribeFeatureType&typename=tows:frida";
-var layerVector = new ol.layer.Vector({
-    title: 'frida',
-    source: new ol.source.Vector({
-        url: '/cgi-bin/proxy.cgi?url='+ encodeURIComponent(u),
+//var url = 'SERVICE=WFS&REQUEST=GetFeature&TYPENAME=' + typeName + '&VERSION=1.1.0&SRSNAME=' + proj;
+//var urlGET = 'http://aspe.local.db0/cgi-bin/proxy.py?url=' + encodeURIComponent(url);
+var urlPOST = 'http://aspe.local.db0/cgi-bin/proxy.py?url=';
 
-        format: new ol.format.WFS({
-
-        })
-    })  
-});
-*/
-
-// Executed when data is loaded by the $.ajax method.
-var loadFeaturesBbox = function(response) {
-    sourceVector.addFeatures(sourceVector.readFeatures(response));
-};
-
-// Source using a bbox strategy
-var sourceVector = new ol.source.Vector({
-    loader: function(extent, resolution, projection) {
-        var url = 'http://aspe.local.db0/cgi-bin/tinyows?' +
-            'service=WFS&' +
-            'request=GetFeature&' +
-            'version=1.1.0&' +
-            'typename=tows:frida&' +
-            'outputFormat=text/javascript&' +
-            'srsname=EPSG:3857&' +
-            'bbox=' + extent.join(',');
-
-        $.ajax({
-            url: url,
-            dataType: 'jsonp'
-        }).then(function(response) {
-            var format = new ol.format.GeoJSON();
-			/*var format = new ol.format.WFS(),*/
-//			var features = ;
-			sourceVector.addFeatures(format.readFeatures(response,
-			    {featureProjection: projection}));
+var formatWFS = new ol.format.WFS();
+sourceVector = new ol.source.Vector({
+    loader: function(extent) {
+        $.ajax(urlPOST, {
+            type: 'GET',
+            data: {
+                service: 'WFS',
+                version: '1.1.0',
+                request: 'GetFeature',
+                typename: 'frida',
+                srsname: 'EPSG:31467',
+                bbox: extent.join(',') + ',EPSG:31467'
+            },
+        }).done(function(response) {
+            sourceVector.addFeatures(formatWFS.readFeatures(response))
         });
     },
-    strategy: ol.loadingstrategy.bbox,
-//    projection: 'EPSG:3857'
+    strategy: ol.loadingstrategy.tile(new ol.tilegrid.createXYZ({
+        maxZoom: 19
+    })),
 });
 
-/*
-//proxyHost = "/cgi-bin/proxy.cgi?url=";
-sourceVector = new ol.source.Vector({
-	loader: function(extent) {
-		$.ajax(proxyHost+'http://aspe.local.db0/cgi-bin/tinyows', {
-			type: 'GET',
-			data: {
-                service: 'WFS',
-				//version: '1.1.0',
-				//request: 'GetFeature',
-				//featureNS:  "http://www.tinyows.org/",
-                typename: 'tows:frida',
-				//srsname: 'EPSG:3857',
-				//bbox: extent.join(',') + ',EPSG:3857',
-                version: "1.1.0",
-                srsName: "EPSG:31467",
-                //url: "http://aspe.local.db0/cgi-bin/tinyows",
-                featureNS :  "http://www.tinyows.org/",
-                //featureType: "frida",
-                //geometryName: "geom",
-                //schema: "http://aspe.local.db0/cgi-bin/tinyows?service=wfs&request=DescribeFeatureType&version=1.1.0&typename=tows:frida"
-			},
-		}).done(function(response) {
-			formatWFS = new ol.format.WFS(),
-			sourceVector.addFeatures(formatWFS.readFeatures(response))
-		});
-	},
-	strategy: ol.loadingstrategy.tile(new ol.tilegrid.XYZ({
-		maxZoom: 19
-	})),
-});
-*/
 var layerVector = new ol.layer.Vector({
-	source: sourceVector
+    source: sourceVector
 });
 
 //hover highlight
 selectPointerMove = new ol.interaction.Select({
-	condition: ol.events.condition.pointerMove
+    condition: ol.events.condition.pointerMove
 });
 
 layerOSM = new ol.layer.Tile({
-	source: new ol.source.OSM()
+    source: new ol.source.OSM()
 });
 
 var map = new ol.Map({
-	target: 'map',
-	//overlays: [overlayPopup],
-	controls: [controlMousePos],
-	layers: [layerOSM, layerVector],
-	view: new ol.View({
-		center: [-9692835, 2347907],
-		zoom: 17
-	})
+    target: 'map',
+    overlays: [overlayPopup],
+    controls: [controlMousePos],
+    layers: [layerOSM, layerVector],
+    view: new ol.View({
+        center: ol.proj.transform([8.056062, 52.272375], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 12
+    })
 });
 map.addInteraction(selectPointerMove);
 
 var interaction;
 var select = new ol.interaction.Select({
-	style: new ol.style.Style({
-		stroke: new ol.style.Stroke({
-			color: '#FF2828'
-		})
-	})
+    style: new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: '#FF2828'
+        })
+    })
 });
 
 //wfs-t
 var dirty = {};
-var formatWFS = new ol.format.WFS();
 var formatGML = new ol.format.GML({
-	//featureNS: 'http://argeomatica.com',
-    featureNS:  "http://www.tinyows.org/",
-	featureType: 'playa_sample',
-	srsName: 'EPSG:3857'
+    featureNS: 'http://www.tinyows.org/',
+    featureType: 'frida',
+    srsName: 'EPSG:31467'
 });
 var transactWFS = function(p, f) {
-	switch(p) {
-	case 'insert':
-		node = formatWFS.writeTransaction([f], null, null, formatGML);
-		break;
-	case 'update':
-		node = formatWFS.writeTransaction(null, [f], null, formatGML);
-		break;
-	case 'delete':
-		node = formatWFS.writeTransaction(null, null, [f], formatGML);
-		break;
-	}
-	s = new XMLSerializer();
-	str = s.serializeToString(node);
-	$.ajax('http://aspe.local.db0/cgi-bin/tinyows',{
-		type: 'POST',
-		dataType: 'xml',
-		processData: false,
-		contentType: 'text/xml',
-		data: str
-	}).done();
+    switch (p) {
+        case 'insert':
+            node = formatWFS.writeTransaction([f], null, null, formatGML);
+            break;
+        case 'update':
+            node = formatWFS.writeTransaction(null, [f], null, formatGML);
+            break;
+        case 'delete':
+            node = formatWFS.writeTransaction(null, null, [f], formatGML);
+            break;
+    }
+
+    if (f === undefined) {
+    	console.log(p, f)
+    } else {
+    	console.log(p, f.getId())
+    }
+
+    s = new XMLSerializer();
+    str = s.serializeToString(node);
+    console.log('**', str);
+    function responseCallback(response) {
+    	var tr = formatWFS.readTransactionResponse(response);
+    	ts = tr.transactionSummary;
+        fid = tr.insertIds;
+        console.log('****', ts, fid);
+        
+        switch (p) {
+    		case 'insert':
+    			if (ts.totalInserted === 1 && fid.length === 1 && fid[0] !== 'none') {
+        			f.setId(fid[0])
+    			}
+				break;
+    		case 'update':
+    			break;
+    		case 'delete':
+    			if (ts.totalDeleted === 1 && fid.length === 1 && fid[0] === 'none') {
+			    	sourceVector.removeFeature(f);		            
+    			}
+    			break;
+    	}
+    }
+    $.ajax(urlPOST, {
+        type: 'POST',
+        dataType: 'xml',
+        processData: false,
+        contentType: 'text/xml',
+        complete: function(xhr, status) {
+        	if (status === 'success') {
+        		responseCallback(xhr.responseXML)
+        	} else {
+        		console.log('STATUS ERROR', status)
+        	}
+        },
+	    error: function(e) {
+	    	var errorMsg = e? (e.status + ' ' + e.statusText) : "";
+	    },
+    
+        data: str
+    }).done();
 }
 
 $('.btn-floating').hover(
-	function() {$(this).addClass('darken-2');},
-	function() {$(this).removeClass('darken-2');}
+    function() { $(this).addClass('darken-2'); },
+    function() { $(this).removeClass('darken-2'); }
 );
 
 $('.btnMenu').on('click', function(event) {
-	$('.btnMenu').removeClass('orange');
-	$(this).addClass('orange');
-	map.removeInteraction(interaction);
-	select.getFeatures().clear();
-	map.removeInteraction(select);
-	switch($(this).attr('id')) {
-	
-	case 'btnSelect':
-		interaction = new ol.interaction.Select({
-			style: new ol.style.Style({
-				stroke: new ol.style.Stroke({color: '#f50057', width: 2})
-			})
-		});
-		map.addInteraction(interaction);
-		/*interaction.getFeatures().on('add', function(e) {
-			props = e.element.getProperties();
-			if (props.status){$('#popup-status').html(props.status);}else{$('#popup-status').html('n/a');}
-			if (props.tiendas){$('#popup-tiendas').html(props.tiendas);}else{$('#popup-tiendas').html('n/a');}
-			coord = $('.ol-mouse-position').html().split(',');
-			overlayPopup.setPosition(coord);
-		});*/
-		break;
-		
-	case 'btnEdit':
-		map.addInteraction(select);
-		interaction = new ol.interaction.Modify({
-			features: select.getFeatures()
-		});
-		map.addInteraction(interaction);
-		
-		snap = new ol.interaction.Snap({
-			source: layerVector.getSource()
-		});
-		map.addInteraction(snap);
-		
-		dirty = {};
-		select.getFeatures().on('add', function(e) {
-			e.element.on('change', function(e) {
-				dirty[e.target.getId()] = true;
-			});
-		});
-		select.getFeatures().on('remove', function(e) {
-			f = e.element;
-			if (dirty[f.getId()]){
-				delete dirty[f.getId()];
-				featureProperties = f.getProperties();
-			    delete featureProperties.boundedBy;
-			    var clone = new ol.Feature(featureProperties);
-			    clone.setId(f.getId());
-			    transactWFS('update', clone);
-			}
-		});
-		break;
-		
-	case 'btnDrawPoint':
-		interaction = new ol.interaction.Draw({
-		    type: 'Point',
-		    source: layerVector.getSource()
-		});
-		map.addInteraction(interaction);
-		interaction.on('drawend', function(e) {
-			transactWFS('insert', e.feature);
-	    });
-		break;
-		
-	case 'btnDrawLine':
-		interaction = new ol.interaction.Draw({
-		    type: 'LineString',
-		    source: layerVector.getSource()
-		});
-		map.addInteraction(interaction);
-		interaction.on('drawend', function(e) {
-			transactWFS('insert', e.feature);
-	    });
-		break;
-		
-	case 'btnDrawPoly':
-		interaction = new ol.interaction.Draw({
-		    type: 'Polygon',
-		    source: layerVector.getSource()
-		});
-		map.addInteraction(interaction);
-		interaction.on('drawend', function(e) {
-			transactWFS('insert', e.feature);
-	    });
-		break;
-		
-	case 'btnDelete':
-		interaction = new ol.interaction.Select();
-		map.addInteraction(interaction);
-		interaction.getFeatures().on('change:length', function(e) {
-			transactWFS('delete', e.target.item(0));
-	        interaction.getFeatures().clear();
-	        selectPointerMove.getFeatures().clear();
-	    });
-		break;
+    $('.btnMenu').removeClass('orange');
+    $(this).addClass('orange');
+    map.removeInteraction(interaction);
+    select.getFeatures().clear();
+    map.removeInteraction(select);
+    switch ($(this).attr('id')) {
 
-	default:
-		break;
-	}
+        case 'btnSelect':
+            interaction = new ol.interaction.Select({
+                style: new ol.style.Style({
+                    stroke: new ol.style.Stroke({ color: '#f50057', width: 2 })
+                })
+            });
+            map.addInteraction(interaction);
+            interaction.getFeatures().on('add', function(e) {
+                props = e.element.getProperties();
+                if (props.status) { $('#popup-status').html(props.status); } else { $('#popup-status').html('n/a'); }
+                if (props.tiendas) { $('#popup-tiendas').html(props.tiendas); } else { $('#popup-tiendas').html('n/a'); }
+                coord = $('.ol-mouse-position').html().split(',');
+                overlayPopup.setPosition(coord);
+                console.log(e.element.getId())
+            });
+            break;
+
+        case 'btnEdit':
+            map.addInteraction(select);
+            interaction = new ol.interaction.Modify({
+                features: select.getFeatures()
+            });
+            map.addInteraction(interaction);
+
+            snap = new ol.interaction.Snap({
+                source: layerVector.getSource()
+            });
+            map.addInteraction(snap);
+
+            dirty = {};
+            select.getFeatures().on('add', function(e) {
+                e.element.on('change', function(e) {
+                    dirty[e.target.getId()] = true;
+                });
+            });
+            select.getFeatures().on('remove', function(e) {
+                f = e.element;
+                if (dirty[f.getId()]) {
+                    delete dirty[f.getId()];
+                    featureProperties = f.getProperties();
+                    delete featureProperties.boundedBy;
+                    var clone = new ol.Feature(featureProperties);
+                    clone.setId(f.getId());
+                	transactWFS('update', clone);
+                }
+            });
+            break;
+
+        case 'btnDrawPoint':
+            interaction = new ol.interaction.Draw({
+                type: 'Point',
+                source: layerVector.getSource()
+            });
+            map.addInteraction(interaction);
+            interaction.on('drawend', function(e) {
+                transactWFS('insert', e.feature);
+            });
+            break;
+
+        case 'btnDrawLine':
+            interaction = new ol.interaction.Draw({
+                type: 'LineString',
+                source: layerVector.getSource()
+            });
+            map.addInteraction(interaction);
+            interaction.on('drawend', function(e) {
+                transactWFS('insert', e.feature);
+            });
+            break;
+
+        case 'btnDrawPoly':
+            interaction = new ol.interaction.Draw({
+                type: 'Polygon',
+                source: layerVector.getSource()
+            });
+            map.addInteraction(interaction);
+            interaction.on('drawend', function(e) {
+                transactWFS('insert', e.feature);
+            });
+            break;
+
+        case 'btnDelete':
+            interaction = new ol.interaction.Select();
+            map.addInteraction(interaction);
+            // interaction.getFeatures().on('change:length', function(e) {
+            interaction.on('select', function(e) {
+                if (e.selected.length == 1) {
+                	transactWFS('delete', e.selected[0]);
+			        interaction.getFeatures().clear();
+			        selectPointerMove.getFeatures().clear();
+            	}                
+            });
+            break;
+
+        default:
+            break;
+    }
 });
 
 $('#btnZoomIn').on('click', function() {
-	var view = map.getView();
-	var newResolution = view.constrainResolution(view.getResolution(), 1);
-	view.setResolution(newResolution);
+    var view = map.getView();
+    var newResolution = view.constrainResolution(view.getResolution(), 1);
+    view.setResolution(newResolution);
 });
 
 $('#btnZoomOut').on('click', function() {
-	var view = map.getView();
-	var newResolution = view.constrainResolution(view.getResolution(), -1);
-	view.setResolution(newResolution);
+    var view = map.getView();
+    var newResolution = view.constrainResolution(view.getResolution(), -1);
+    view.setResolution(newResolution);
 });
